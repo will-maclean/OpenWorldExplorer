561e1d33ba615c7d88840797ea264380648e74c0
Will Maclean <wmac0001@student.monash.edu>
2020-07-11 13:02:36 +1000

    Added a TileChunkTest class

---
+++ src/test/java/wmaclean/tile/TileChunkTest.java
@@ -0,0 +1,20 @@
+package wmaclean.tile;
+
+import org.junit.Test;
+import wmaclean.Game;
+
+import static org.junit.Assert.*;
+
+public class TileChunkTest {
+
+    @Test
+    public void testToString() {
+        Game game = new Game();
+
+        TileChunk tileChunk = new TileChunk(0, 0, game);
+
+        String str = tileChunk.toString();
+
+        assertEquals("(0.0, 0.0) (256.0, 0.0) (0.0, 256.0) (256.0, 256.0)", str);
+    }
+}
